fe12ea065375029fcc746f3f5ebce3eefc84da48
hyesungpark <jayhspark@gmail.com>
2023-08-07 22:48:53 +0900

    make cardview

---
+++ Hike/Component/CardView.swift
@@ -0,0 +1,41 @@
+//
+//  CardView.swift
+//  Hike
+//
+//  Created by Hye Sung Park on 2023/08/07.
+//
+
+import SwiftUI
+
+struct CardView: View {
+  var body: some View {
+    // MARK: - CARD
+    
+    
+    ZStack {
+      CustomBackgroundView()
+      
+      ZStack {
+        Circle()
+          .fill(
+            LinearGradient(
+              colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")],
+              startPoint: .topLeading,
+              endPoint: .bottomTrailing)
+          )
+          .frame(width: 256, height: 256)
+        
+        Image("image-1")
+          .resizable()
+          .scaledToFit()
+      }
+    } //: CARD
+    .frame(width: 320, height: 570)
+  }
+}
+
+struct CardView_Previews: PreviewProvider {
+  static var previews: some View {
+    CardView()
+  }
+}
