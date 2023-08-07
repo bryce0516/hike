747c9122d997a35caa5760f327bac5f8278d29a9
hyesungpark <jayhspark@gmail.com>
2023-08-07 21:56:51 +0900

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Hye Sung Park on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
