3d5c13af6ef06a2dbb2fcd27d93015354ef5e76c
hyesungpark <jayhspark@gmail.com>
2023-08-13 20:58:41 +0900

    update

--- Hike/Component/CustomListRowView.swift
+++ Hike/Component/CustomListRowView.swift
@@ -8,31 +8,43 @@
 import SwiftUI
 
 struct CustomListRowView: View {
-    var body: some View {
-      LabeledContent {
-        Text("Hike")
-          .foregroundColor(.primary)
-          .fontWeight(.heavy)
-      } label: {
-        HStack {
-          ZStack {
-            RoundedRectangle(cornerRadius: 8)
-              .frame(width: 30, height: 30)
-              .foregroundColor(.blue)
-            Image(systemName: "apps.iphone")
-              .foregroundColor(.white)
-              .fontWeight(.semibold)
-          }
-          Text("Application")
+  // MARK: - PROPERTIES
+  
+  @State var rowLabel: String
+  @State var rowIcon: String
+  @State var rowContent: String
+  @State var rowTintColor: Color
+  
+  var body: some View {
+    LabeledContent {
+      Text("Hike")
+        .foregroundColor(.primary)
+        .fontWeight(.heavy)
+    } label: {
+      HStack {
+        ZStack {
+          RoundedRectangle(cornerRadius: 8)
+            .frame(width: 30, height: 30)
+            .foregroundColor(.blue)
+          Image(systemName: "apps.iphone")
+            .foregroundColor(.white)
+            .fontWeight(.semibold)
         }
+        Text("Application")
       }
     }
+  }
 }
 
 struct CustomListRowView_Previews: PreviewProvider {
-    static var previews: some View {
-      List {
-        CustomListRowView()
-      }
+  static var previews: some View {
+    List {
+      CustomListRowView(
+        rowLabel: "Designer",
+        rowIcon: "paintpalette",
+        rowContent: "John Doe",
+        rowTintColor: .pink
+      )
     }
+  }
 }
